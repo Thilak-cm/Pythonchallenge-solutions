df319297a636b180b7956461dc01571c9b274f94
Thilak CM <62937393+Thilak-cm@users.noreply.github.com>
2021-05-12 11:29:17 +0530

    Add files via upload

---
+++ pych1.py
@@ -0,0 +1,14 @@
+#PythonChallenge 1:
+
+puzzle = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+
+orig_alphabets = "abcdefghijklmnopqrstuvwxyz"
+encoded_aphabets = "cdefghijklmnopqrstuvwxyzab"
+table = puzzle.maketrans(orig_alphabets, encoded_aphabets)
+
+decoded_puzzle = puzzle.translate(table)
+print(decoded_puzzle)
+
+#Now change puzzle to "map"
+#This gives you "ocr"
+#Apply this in the url to progess to the next level
