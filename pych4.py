b90ff322e55a52435ff30f66e288db11871e2147
Thilak CM <62937393+Thilak-cm@users.noreply.github.com>
2021-05-18 09:04:31 +0530

    Update pych4.py
    used string formatting to change link value for every iteration

--- pych4.py
+++ pych4.py
@@ -1,51 +1,38 @@
-#PYTHONCHALLENGE 4
-
+# PYTHONCHALLENGE 4
 import re
 from urllib.request import urlopen
 
-link = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345"
-
-#this function obtains the next nothing value
-def get_nothing(content, i):
-    current_nothing = re.search("\d+", link)
-    next_nothing = re.search("next nothing is (\d+)", content)
-    next_nothing = re.search("\d+", next_nothing.group())
+link = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing={}"
+next_nothing = 12345  # this  is the first nothing value
 
 
+# this value reads contents of the link
+def read_content(url):
+    f = urlopen(url)
+    page_source = f.read().decode()
+    return page_source
 
-    return current_nothing.group(), next_nothing.group()
 
-#this value reads contents of the link
-def read_content(link):
-    f = urlopen(link)
-    content = f.read().decode()
-    return content
-
-i = 0
 while True:
-    content = read_content(link)
-    
-    #this if case covers the possibility of the content having no numbers in it 
-    #this occurs twice before we reach the answer
-    if re.search("\d+" ,content) == None:
-        print("in if loop")
-        current_nothing = re.search("\d+", link)
-        next_nothing = int(current_nothing.group()) // 2
-        link = link.replace(current_nothing.group(), str(next_nothing))
-        print("and the next nothing is " + str(next_nothing))
-        
-        #this if case checks if html is in the content, because if it is, then we have reached our answer 
+    content = read_content(link.format(next_nothing))
+
+    # this if case covers the possibility of the content having no numbers in it
+    if re.search("\d+", content) == None:
+
+        # this if case checks if html is in the content, because if it is, then we have reached our answer
         if "html" in content:
             print(content)
             break
-        i +=1
         continue
 
-    current_nothing, next_nothing = get_nothing(content, i)
-    link = link.replace(current_nothing, next_nothing)
+        print(content)
+        next_nothing = int(next_nothing) // 2  # content of this link says Divide by two to get next nothing
+        link.format(next_nothing)
+        print("and the next nothing is " + str(next_nothing))
+
+    match = re.search("next nothing is (\d+)", content)
+    next_nothing = match.group(1)
+    link.format(next_nothing)
     print("and the next nothing is " + next_nothing)
 
-    print(f"ith iteration:{i}")
-    i +=1
-
-#Take the output and past it in the url to progress to the next level
+# Take the output and past it in the url to progress to the next level
