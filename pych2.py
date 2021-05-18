b499abc32cf04ae2fc0215aff09ba793b2799f93
Thilak CM <62937393+Thilak-cm@users.noreply.github.com>
2021-05-18 08:35:37 +0530

    Update pych2.py
    removed Counter and used re instead to find the letters in the page source's comments

--- pych2.py
+++ pych2.py
@@ -1,7 +1,6 @@
 # PYTHONCHALLENGE LEVEL 2
 
 from urllib.request import urlopen
-from collections import Counter
 import re
 
 # url of the current pythonchallenge puzzle
@@ -16,16 +15,9 @@ data = re.findall("<!--(.*?)-->", page_source, re.DOTALL)[-1]
 # this gives a two element list an an output; hence we take the last element
 # as the first one contains the question ---> find rare characters in the mess below:
 
-frequency = Counter(data).most_common()
-frequency = tuple(frequency)
-# converted to its tuple form so as to enable tuple matching done below
+pattern = re.compile("[a-z]") # to find all the letters in data
+rare_letters = pattern.findall(data)
+print(rare_letters)
 
-rare_letters = []
-i = 0
-for str1, f in frequency:
-    i += 1
-    if f == 1:
-        rare_letters.append(frequency[i-1][0])
-
-print("".join(rare_letters))
+print("".join(rare_letters)) # joining all the elements of the list
 # copy the output and paste it in the url to progress to the next level
