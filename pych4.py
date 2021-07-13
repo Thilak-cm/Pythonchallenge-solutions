033b8174f6836520a6a278ae7d19bac7c851355e
Thilak CM <62937393+Thilak-cm@users.noreply.github.com>
2021-07-13 10:30:59 +0530

    Update pych4.py
    fixed an error with the continue statement 
    also added function annotations to read_content

--- pych4.py
+++ pych4.py
@@ -7,7 +7,7 @@ next_nothing = 12345  # this  is the first nothing value
 
 
 # this value reads contents of the link
-def read_content(url):
+def read_content(url: str) -> str:
     f = urlopen(url)
     page_source = f.read().decode()
     return page_source
@@ -23,12 +23,12 @@ while True:
         if "html" in content:
             print(content)
             break
-        continue
 
         print(content)
         next_nothing = int(next_nothing) // 2  # content of this link says Divide by two to get next nothing
         link.format(next_nothing)
         print("and the next nothing is " + str(next_nothing))
+        continue
 
     match = re.search("next nothing is (\d+)", content)
     next_nothing = match.group(1)
